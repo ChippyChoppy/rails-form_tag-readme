887cb4dde4f9a0c5f735f61cbb7f142b465645b8
ChippyChoppy <rebeccamargaret13@gmail.com>
2020-10-28 17:55:56 -0400

    commit tree 995eff5d2c23b9cd81afc35f5566884fdc8d9c31

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -5,4 +5,16 @@ describe 'new post' do
         visit new_post_path
         expect(page.status_code).to eq(200)
     end
+
+    it "displays a new post form that redirects to the index page, which then contains the submitted post's title and description" do 
+        visit new_post_path 
+        fill_in 'post_title', with: 'Hecking Dinosaurs, Ammiright?!'
+        fill_in 'post description', with: 'All about how kewl dinosaurs are'
+
+        click_on 'Submit Post'
+
+        expect(page.current_path).to eq(posts_path)
+        expect(page).to have_content('Hecking Dinosaurs, Ammiright?!')
+        expect(page).to have_content('All about how kewl dinosaurs are')
+    end
 end
